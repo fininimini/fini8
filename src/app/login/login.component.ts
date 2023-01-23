0ebcb808fab0c4c9e3ebec89e9c26aeb49b1af87
fininimini <markusskalenda@gmail.com>
2023-01-24 00:12:20 +0200

    1. Added email incorrect hint message 2. Added password security level bar 3. Other cleanup, bugfixes and imporvements

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,137 +11,119 @@ import { LoadingComponent } from "../loading/loading.component";
 
 export class LoginComponent {
     pswdRevealed = false;
-    revealPassword(event: Event, subID: string): void {
-        event.preventDefault();
-        const reveal: HTMLImageElement = document.getElementById("revealPasswordImage" + subID) as HTMLImageElement;
-        const hide: HTMLImageElement = document.getElementById("hidePasswordImage" + subID) as HTMLImageElement;
-        const pswdInput: HTMLInputElement = document.getElementById("passwordInput" + subID) as HTMLInputElement;
-        if (!this.pswdRevealed) {
-            reveal.style.display = "none";
-            hide.style.display = "";
-            pswdInput.type = "text";
-            this.pswdRevealed = true;
-        } else {
-            reveal.style.display = "";
-            hide.style.display = "none";
-            pswdInput.type = "password";
-            this.pswdRevealed = false;
-        }
-    }
-
+    loginEmail = ""
+    emailMessageRevealed = false;
     registerEmail = "";
     emailValid = false;
-    checkEmail(email: string, hover = false): void {
-        const emailInput: HTMLInputElement = document.getElementById("emailInputRegistration") as HTMLInputElement;
-        const errorDiv: HTMLDivElement = document.getElementById("emailError") as HTMLDivElement;
-        const valid: boolean = validate(email);
+    loginPswd = "";
+    registerPswd = "";
+    pswdValid = false;
 
-        if (errorDiv != null && !valid && email.length != 0) {
-            if (document.activeElement == emailInput || hover) {
-                errorDiv.style.maxHeight = "100px";
-            } else {
-                errorDiv.style.maxHeight = "0";
-            }
+    revealPassword(event: Event, subID: string): void {
+        event.preventDefault();
+        const reveal = document.getElementById("revealPasswordImage" + subID) as HTMLImageElement;
+        const hide = document.getElementById("hidePasswordImage" + subID) as HTMLImageElement;
+        const pswdInput = document.getElementById("passwordInput" + subID) as HTMLInputElement;
+        reveal.style.display = !this.pswdRevealed ? 'none' : '';
+        hide.style.display = !this.pswdRevealed ? '' : 'none';
+        pswdInput.type = !this.pswdRevealed ? 'text' : 'password';
+        this.pswdRevealed = !this.pswdRevealed;
+    }
+    revealEmailMessage(event: Event | null = null, hide = false, onlyHide = false, set = false, setValue = false): void {
+        const revealImg = document.getElementById("revealEmailMessage") as HTMLImageElement;
+        const hideImg = document.getElementById("hideEmailMessage") as HTMLImageElement;
+        const errorDiv = document.getElementById("emailError") as HTMLDivElement;
+        const emailButton = document.getElementById("revealEmail") as HTMLButtonElement;
+        if (event != null) {event.preventDefault()}
+        emailButton.disabled = hide;
+        emailButton.style.display = hide ? 'none' : '';
+        if (!onlyHide) {
+            const revealState = (set && setValue) || (!set && !this.emailMessageRevealed);
+            revealImg.style.display = revealState ? 'none' : '';
+            hideImg.style.display = revealState ? '' : 'none';
+            this.emailMessageRevealed = revealState;
+            errorDiv.style.maxHeight = revealState ? '100px' : '0';
+        }
+    }
+    checkEmail(): void {
+        const emailInput = document.getElementById("emailInputRegistration") as HTMLInputElement;
+        const valid = validate(this.registerEmail);
+        if (this.registerEmail.length > 0) {
+            this.emailValid = valid;
+            emailInput.style.borderColor = valid ? "#00ffaad8" : "#ff4b4bd8";
+            this.revealEmailMessage(null, valid, false, true);
         } else {
-            const errorDiv: HTMLDivElement = document.getElementById("emailError") as HTMLDivElement;
-
-            if (email.length == 0) {
-                emailInput.style.borderColor = "#8f8f8f";
-                if (errorDiv != null) {
-                    errorDiv.style.maxHeight = "0";
-                    setTimeout(function() {errorDiv.remove()}, 600);
-                }
-                this.emailValid = false;
-            } else if (valid) {
-                emailInput.style.borderColor = "#00ffaad8";
-                if (errorDiv != null) {
-                    errorDiv.style.maxHeight = "0";
-                    setTimeout(function() {errorDiv.remove()}, 600);
-                    this.emailValid = true;
-                }
-            } else {
-                emailInput.style.borderColor = "#ff4b4bd8";
-                const parrentDiv: HTMLDivElement = document.getElementById("emailDiv") as HTMLDivElement;
-                let errorDiv: HTMLDivElement = document.createElement("div") as HTMLDivElement;
-
-                errorDiv.id = "emailError";
-                errorDiv.innerHTML = '<div style="padding: 3px 0 3px 0;">Please enter a valid email address following this format: name@domain-name.domain</div>';
-                errorDiv.setAttribute("style", `transition: max-height 0.5s ease-out; border-left: #ff4b4bd8 3px solid; background-color: #ff4b4b11; box-sizing: border-box; padding-left: 10px; position: relative; overflow: hidden; margin-left: 3px; color: #ff4b4b; max-height: 0; bottom: 15px; width: ${parseInt(getComputedStyle(parrentDiv).width) - 3}px;`);
-                parrentDiv.appendChild(errorDiv);
-                errorDiv = document.getElementById("emailError") as HTMLDivElement;
-                setTimeout(function() {errorDiv.style.maxHeight = "100px";}, 10);
-                this.emailValid = false;
-            }
+            this.emailValid = false;
+            emailInput.style.borderColor = "#8f8f8f";
+            this.revealEmailMessage(null, true, false, true);
         }
         this.checkCredentials();
     }
-
     onSubmit(event: Event): void {
         const loadingComponent = new LoadingComponent();
         loadingComponent.loadingActivate(event);
     }
-
-    loginEmail = ''
     checkCredentials(): void {
-        const registerBtn: HTMLButtonElement = document.getElementById("register") as HTMLButtonElement;
-        const loginBtn: HTMLButtonElement = document.getElementById("login") as HTMLButtonElement;
-        if (this.pswdValid && this.emailValid) {
-            registerBtn.removeAttribute("disabled");
-        } else {
-            registerBtn.setAttribute("disabled", "");
-        }
-        if (this.loginPswd.length > 0 && this.loginEmail.length > 0) {
-            loginBtn.removeAttribute("disabled");
-        } else {
-            loginBtn.setAttribute("disabled", "");
-        }
-    }
-
-    loginPswd = "";
-    registerPswd = "";
-    pswdValid = false;
-    checkPswd(registerPswd: string, hover = true): void {
-        const pswdInput: HTMLInputElement = document.getElementById("passwordInputRegistration") as HTMLInputElement;
-        const pswdDiv: HTMLDivElement = document.getElementById("pswd_div") as HTMLDivElement;
-        const criterias: Array < [string, boolean] > = [
-            ["#pswd_spaces", registerPswd.includes(" ") == false && registerPswd.length >= 1],
-            ["#pswd_min_char", registerPswd.length >= 8],
-            ["#pswd_big_char", /[A-Z]/.test(registerPswd)],
-            ["#pswd_sml_char", /[a-z]/.test(registerPswd)],
-            // eslint-disable-next-line no-useless-escape
-            ["#pswd_spc_char", /\d/.test(registerPswd) || /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(registerPswd)]
+        const registerBtn = document.getElementById("register") as HTMLButtonElement;
+        const loginBtn = document.getElementById("login") as HTMLButtonElement;
+        const requirements: Array<[boolean, HTMLButtonElement]> = [
+            [(this.pswdValid && this.emailValid), registerBtn],
+            [this.loginPswd.length > 0 && this.loginEmail.length > 0, loginBtn]
         ];
-
-        for (const criteria of criterias) {
-            const dot: HTMLDivElement = document.querySelector(criteria[0]) as HTMLDivElement;
-            if (criteria[1]) {
-                dot.style.backgroundColor = "#00ffaad8";
-            } else if (registerPswd.length == 0) {
-                dot.style.backgroundColor = "#8f8f8f";
-            } else {
-                dot.style.backgroundColor = "#ff4b4bd8";
-            }
+        requirements.forEach(([valid, btn]) => btn.disabled = !valid);
+    }
+    checkPswd(): void {
+        const pswdInputDiv = document.getElementById("passwordInputRegistrationDiv") as HTMLDivElement;
+        const pswdInput = document.getElementById("passwordInputRegistration") as HTMLInputElement;
+        const criterias: Array<[boolean, number]> = [
+            [this.registerPswd.length >= 8, 1],
+            [this.registerPswd.length >= 10, 1],
+            [/[A-Z]/.test(this.registerPswd), 1],
+            [/[a-z]/.test(this.registerPswd), 1],
+            // eslint-disable-next-line no-useless-escape
+            [/[`!@#$%^&*()_+\-=\[\]{};":"\\|,.<>\/?~]/.test(this.registerPswd), 1],
+            [/\d/.test(this.registerPswd), 1]
+        ];
+        const levels: {[name: number]: {color: string, text: string}} = {
+            0: {"color": "#ff4b4bd8", "text": "Too Weak"},
+            1: {"color": "#ff4b4bd8", "text": "Weak"},
+            2: {"color": "#FAA916d8", "text": "Fair"},
+            3: {"color": "#00FFAAd8", "text": "Good"},
+            4: {"color": "#00FFAAd8", "text": "Excelent"}
+        };
+        let score = 0;
+        criterias.forEach(element => {if(element[0]){score += element[1]}});
+        const levelDispaly = (level: number) => {
+            pswdInputDiv.style.borderColor = levels[level]["color"];
+            (document.getElementById("levelTxt") as HTMLSpanElement).innerHTML = levels[level]["text"]
+            const levelElements = [
+                document.getElementById("weakLevel") as HTMLDivElement,
+                document.getElementById("fairLevel") as HTMLDivElement,
+                document.getElementById("goodLevel") as HTMLDivElement,
+                document.getElementById("excelentLevel") as HTMLDivElement
+            ];
+            levelElements.forEach((elem, index) => elem.style.backgroundColor = level >= index + 1 ? levels[level]["color"] : "#8f8f8f");
         }
-
-        if (criterias.every(criteria => criteria[1] === true)) {
-            pswdDiv.style.borderColor = "#00ffaad8";
-            pswdInput.style.borderColor = "#00ffaad8";
-            pswdDiv.style.maxHeight = "0";
-            this.pswdValid = true;
-        } else if (registerPswd.length == 0) {
-            pswdDiv.style.borderColor = "#8f8f8f";
-            pswdInput.style.borderColor = "#8f8f8f";
-            pswdDiv.style.maxHeight = "0";
+        if (this.registerPswd.length === 0) {
+            if (document.activeElement === pswdInput) {
+                levelDispaly(0);
+            } else {
+                pswdInputDiv.style.borderColor = "#8f8f8f";
+            }
+        } else if (this.registerPswd.length >= 6) {
+            levelDispaly(score >= 4 && score < 5 ? 2 : score >= 5 && score < 6 ? 3 : score === 6 ? 4 : 1);
+            this.pswdValid = score >= 4;
+        } else {
+            levelDispaly(0);
+            this.pswdValid = false;
+        }
+        (document.getElementById("pswd_div") as HTMLDivElement).style.maxHeight = (this.pswdValid || this.registerPswd.length === 0) && document.activeElement !== pswdInput ? "0" : "100px";
+        if (this.registerPswd.includes(" ")) {
+            (document.getElementById("pswdError") as HTMLDivElement).style.maxHeight = "100px";
+            pswdInputDiv.style.borderColor = "#ff4b4bd8";
             this.pswdValid = false;
         } else {
-            pswdDiv.style.borderColor = "#ff4b4bd8"
-            pswdInput.style.borderColor = "#ff4b4bd8";
-            if (document.activeElement == pswdInput || hover) {
-                pswdDiv.style.maxHeight = "100px";
-            } else {
-                pswdDiv.style.maxHeight = "0";
-            }
-            this.pswdValid = false;
+            (document.getElementById("pswdError") as HTMLDivElement).style.maxHeight = "0";
         }
         this.checkCredentials();
     }
