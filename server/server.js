973d611ee6e0190b55fb31f672e213e9dfc2a99a
fininimini <52744933+fininimini@users.noreply.github.com>
2023-02-11 22:33:30 +0200

    Login dev (#9)
    * Started making the login service
    
    * Moved handle_data handling type specific data
    
    Moved handle_data handling type specific data to data object
    
    * Update server.js
    
    * Update server.js
    
    * Update server.js
    
    * cleanup, bugfixes, continued making login
    
    * Imporved pswd error
    
    * Cleanup,backend bugfix, switched from sass to scss
    
    * added notifications
    
    * Setup registration and password hashing

--- server/server.js
+++ server/server.js
@@ -1,27 +1,96 @@
-express = require('express');
-app = express();
-port = process.env.PORT || 8080;
-dir = __dirname + "/fini8"
-
-app.get('/', function(req, res) {
-    res.sendFile(dir + '/index.html');
-});
-app.get('/login', function(req, res) {
-    res.sendFile(dir + '/index.html');
-});
-app.get('/404', function(req, res) {
-    res.sendFile(dir + '/index.html');
-});
-app.get('/favicon.ico', function(req, res) {
-    res.sendFile(dir + '/favicon.ico');
-});
-app.use('/staticAssets', express.static(dir));
-app.use('/assets', express.static(dir + "/assets"));
 /* eslint-env es6 */
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const { pbkdf2, timingSafeEqual, randomBytes } = require('crypto');
+
+const port = process.env.PORT || 8080;
+const dir = __dirname + "/fini8";
+const mongoURI = '***REMOVED***';
+
+const app = express();
+const mongoClient = new MongoClient(mongoURI);
+
+app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.status(404).redirect("404")
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    if (req.method === 'OPTIONS') {
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.get('/', (req, res) => res.sendFile(dir + '/index.html'));
+app.get('/login', (req, res) => res.sendFile(dir + '/index.html'));
+app.get('/404', (req, res) => res.sendFile(dir + '/index.html'));
+app.get('/favicon.ico', (req, res) => res.sendFile(dir + '/favicon.ico'));
+
+app.post('/handle_data', async (req, res) => {
+    if (req.body["type"] === "login") {
+        if (!req.body.hasOwnProperty("data") || !["email", "pswd"].every(key => req.body["data"].hasOwnProperty(key))) {
+            res.status(200).json({status: 400, message: "Bad Request"});
+            return;
+        }
+        try {
+            await mongoClient.connect();
+        } catch (error) {
+            console.error('Error: MongoDB connection refused');
+            res.status(200).json({status: 503, accepted: false, message: "Internal Service Unavailable"});
+            return;
+        }
+        const user = await mongoClient.db("Website").collection('Users').findOne({email: req.body["data"]["email"]});
+        await mongoClient.close();
+        if (user === null) {
+            res.status(200).json({status: 401, accepted: false, message: "Invalid Credentials" });
+        } else if (user["email"] === req.body["data"]["email"]) {
+            pbkdf2(req.body["data"]["pswd"], user["pswd"]["salt"], 1000000, 32, 'sha256', (err, derivedKey) => {
+                if (timingSafeEqual(Buffer.from(user["pswd"]["hash"], 'hex'), derivedKey)) res.status(200).json({status: 200, accepted: true});
+                else res.status(200).json({status: 401, accepted: false, message: "Invalid Credentials"});
+            });
+        } else {
+            res.status(200).json({status: 401, accepted: false, message: "Invalid Credentials"});
+        }
+    } else if (req.body["type"] === "register") {
+        if (!req.body.hasOwnProperty("data") || !["email", "pswd"].every(key => req.body["data"].hasOwnProperty(key))) {
+            res.status(200).json({status: 400, message: "Bad Request"});
+            return;
+        }
+        try {
+            await mongoClient.connect();
+        } catch (error) {
+            console.error('Error: MongoDB connection refused');
+            res.status(200).json({status: 503, accepted: false, message: "Internal Service Unavailable"});
+            return;
+        }
+        const user = await mongoClient.db("Website").collection('Users').findOne({email: req.body["data"]["email"]});
+        await mongoClient.close();
+        if (user !== null) {
+            res.status(200).json({status: 409, accepted: false, message: "Account With Given Credentials Already Exists"});
+        } else {
+            try {
+                await mongoClient.connect();
+            } catch (error) {
+                console.error('Error: MongoDB connection refused');
+                res.status(200).json({status: 503, accepted: false, message: "Internal Service Unavailable"});
+                return;
+            }
+            const salt = randomBytes(Math.ceil(8)).toString('hex').slice(0, 16);
+            await pbkdf2(req.body["data"]["pswd"], salt, 1000000, 32, 'sha256', async (err, derivedKey) => {
+                await mongoClient.db("Website").collection('Users').insertOne({email: req.body["data"]["email"], pswd: {hash: derivedKey.toString('hex'), salt: salt}});
+                await mongoClient.close();
+                res.status(200).json({status: 201, accepted: true, message: "Account Created Successfully"});
+            });
+        }
+    } else {
+        await res.status(200).json({status: 405, accepted: false, message: "Data Handling Method Not Allowed"});
+    }
 })
 
-app.listen(port, function () {
-    console.log('Server running on port ' + port);
-});
+app.use('/staticAssets', express.static(dir));
+app.use('/assets', express.static(dir + "/assets"));
+
+app.use((req, res, next) => res.status(404).redirect("404"));
+
+app.listen(port, () => console.log('Server running on port ' + port))
